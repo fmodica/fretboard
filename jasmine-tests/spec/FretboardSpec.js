3b0b7a1ad2070ba627f65fbdc739abb8590ae695
Frank Modica <fmodica85@gmail.com>
2015-05-27 17:16:07 -0400

    more scenarios for clicked notes

--- jasmine-tests/spec/FretboardSpec.js
+++ jasmine-tests/spec/FretboardSpec.js
@@ -3,6 +3,8 @@
 describe("Fretboard", function() {
     var $fretboard;
     var fretboardInstance;
+    var eightStringTuning;
+    var standardTuning;
 
     beforeEach(function() {
         setFixtures("<div class='my-fretboard-js'></div>");
@@ -10,10 +12,8 @@ describe("Fretboard", function() {
         $fretboard = $(".my-fretboard-js");
         $fretboard.fretboard();
         fretboardInstance = $fretboard.data('fretboard');
-    });
 
-    describe("Default configuration", function() {
-        var standardTuning = [{
+        eightStringTuning = [{
             letter: "E",
             octave: 4
         }, {
@@ -31,12 +31,30 @@ describe("Fretboard", function() {
         }, {
             letter: "E",
             octave: 2
+        }, {
+            letter: "B",
+            octave: 1
+        }, {
+            letter: "F#/Gb",
+            octave: 1
         }];
-        var numFrets = 15;
-        var noteCircles = [3, 5, 7, 9, 12, 15, 17, 19, 21, 24];
-        var noteLetters = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "Ab/G#", "A", "A#/Bb", "B"];
-        var animationSpeed = 500;
-        
+
+        standardTuning = $.extend(true, [], eightStringTuning).slice(0, 6);
+    });
+
+    describe("Default configuration", function() {
+        var numFrets;
+        var noteCircles;
+        var noteLetters;
+        var animationSpeed;
+
+        beforeEach(function() {
+            numFrets = 15;
+            noteCircles = [3, 5, 7, 9, 12, 15, 17, 19, 21, 24];
+            noteLetters = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "Ab/G#", "A", "A#/Bb", "B"];
+            animationSpeed = 500;
+        });
+
         it("should have standard tuning", function() {
             expect(fretboardInstance.getTuning()).toEqual(standardTuning);
         });
@@ -73,48 +91,49 @@ describe("Fretboard", function() {
     });
 
     describe("Clicking notes", function() {
-        var cMaj7Notes = [{
-            stringItsOn: {
-                letter: "E",
-                octave: 4
-            },
-            fretNumber: 3
-        }, {
-            stringItsOn: {
-                letter: "B",
-                octave: 3
-            },
-            fretNumber: 5,
-        }, {
-            stringItsOn: {
-                letter: "G",
-                octave: 3
-            },
-            fretNumber: 4
-        }, {
-            stringItsOn: {
-                letter: "D",
-                octave: 3
-            },
-            fretNumber: 5
-        }, {
-            stringItsOn: {
-                letter: "A",
-                octave: 2
-            },
-            fretNumber: 3
-        }, {
-            stringItsOn: {
-                letter: "E",
-                octave: 2
-            },
-            fretNumber: 3
-        }];
+        var clickedNotes;
+        var expectedClickedNotes;
 
-        it("should show the correct clicked Notes ", function() {
-            fretboardInstance.setClickedNotes(cMaj7Notes);
+        beforeEach(function() {
+            clickedNotes = [{
+                stringItsOn: {
+                    letter: "E",
+                    octave: 4
+                },
+                fretNumber: 3
+            }, {
+                stringItsOn: {
+                    letter: "B",
+                    octave: 3
+                },
+                fretNumber: 5,
+            }, {
+                stringItsOn: {
+                    letter: "G",
+                    octave: 3
+                },
+                fretNumber: 4
+            }, {
+                stringItsOn: {
+                    letter: "D",
+                    octave: 3
+                },
+                fretNumber: 5
+            }, {
+                stringItsOn: {
+                    letter: "A",
+                    octave: 2
+                },
+                fretNumber: 3
+            }, {
+                stringItsOn: {
+                    letter: "E",
+                    octave: 2
+                },
+                fretNumber: 3
+            }];
 
-            var expectedClickedNotes = $.extend(true, [], cMaj7Notes);
+            expectedClickedNotes = $.extend(true, [], clickedNotes);
 
             expectedClickedNotes[0].letter = "G";
             expectedClickedNotes[0].octave = 4;
@@ -128,12 +147,34 @@ describe("Fretboard", function() {
             expectedClickedNotes[4].octave = 3;
             expectedClickedNotes[5].letter = "G";
             expectedClickedNotes[5].octave = 2;
-
-            var clickedNotes = fretboardInstance.getClickedNotes();
-
-            expect(clickedNotes).toEqual(expectedClickedNotes);
         });
 
+        it("should show the correct clicked notes when there are less notes than strings", function() {
+            fretboardInstance.setClickedNotes(clickedNotes);
+
+            expect(fretboardInstance.getClickedNotes()).toEqual(expectedClickedNotes);
+        });
+
+        it("should show the correct clicked notes when there are more notes than strings", function() {
+            fretboardInstance.setTuning(standardTuning.slice(0,1));
+            fretboardInstance.setClickedNotes(clickedNotes);
+
+            expect(fretboardInstance.getClickedNotes()).toEqual(expectedClickedNotes.slice(0,1));
+        });
+
+        it("should show the correct clicked notes when notes are clicked and the number of strings is decreased", function() {
+            fretboardInstance.setClickedNotes(clickedNotes);
+            fretboardInstance.setTuning(standardTuning.slice(0,1));
+
+            expect(fretboardInstance.getClickedNotes()).toEqual(expectedClickedNotes.slice(0,1));
+        });
+
+        it("should show the correct clicked notes when notes are clicked and the number of strings is increased", function() {
+            fretboardInstance.setClickedNotes(clickedNotes);
+            fretboardInstance.setTuning(eightStringTuning);
+
+            expect(fretboardInstance.getClickedNotes()).toEqual(expectedClickedNotes);
+        });
     });
 
     afterEach(function() {
